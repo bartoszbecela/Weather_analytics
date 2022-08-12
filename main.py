b51bb282f33d90070264a6346b1044e8a9ea43b1
bartoszbecela <bartoszbecela1@gmail.com>
2022-08-12 16:21:27 +0200

    New comments added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import csv
+
 import pandas as pd
 from requests import get
 from json import loads
@@ -13,42 +13,49 @@ def main():
     url = 'https://danepubliczne.imgw.pl/api/data/synop/'
     response = get(url)
     rows = [
-        ['Miasto', 'Godzna_pomiaru', 'Temperatura','Cisnienie','Suma_opadu']   # enter parameters which you want to know(your names)
+        ['Id_stacji','Miasto', 'Godzna_pomiaru', 'Temperatura','Cisnienie','Suma_opadu','predkosc_wiatru','kierynek_wiatru','wiglotnosc']   # enter parameters which you want to know(your names)
     ]
     for row in loads(response.text):
         if row['stacja'] in CITTIES:
           rows.append([                     # In this list enter original names of parameters which are from IMGW site
+              row['id_stacji'],
               row['stacja'],
               row['godzina_pomiaru'],
               row['temperatura'],
               row['cisnienie'],
-              row['suma_opadu']
+              row['suma_opadu'],
+              row['predkosc_wiatru'],
+              row['kierunek_wiatru'],
+              row['wilgotnosc_wzgledna']
           ])
 
-    # print(rows)
+    #
     table = AsciiTable(rows)
     print(table.table)
     df = pd.DataFrame(rows)
     print(df)
 
+    # Alternative method of saving data to csv file( save data as string)
 
     # with open('/home/bartosz/Dokumenty/weather.csv', 'a') as csv_file:
     #      csv_writer = csv.writer(csv_file, delimiter=',')
     #      csv_writer.writerow(rows)
-    df.to_csv('/home/bartosz/Dokumenty/weather.csv',mode= 'a', index= False)                # Enter path where file will be save
+
+    # Enter path where file will be save
+    df.to_csv('/home/bartosz/Dokumenty/weather.csv',mode= 'a', index= False)
 
 
 if __name__ == '__main__':
-    print('Everything is ok')
+    print('Data are saved')
     main()
 
+# Scheduler starts every hour at xx.05
 schedule.every().hour.at(":05").do(main)
-a = 0
+
 while True:
     schedule.run_pending()
     time.sleep(1)
-    a += 1
-    print(a)
+
 
 
 
